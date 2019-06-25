8ead8c9db255f202c04b2ed566f724d6b3d1be34
Michael Tan <michael.t@weefer.co.id>
2019-06-25 14:04:45 +0700

    update question + update lesson 4 & 5

---
+++ Lesson 4 - Counting Elements/MaxCounters/MaxCounters C# 88%.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Linq;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+// By MichaelKosuke@github
+// https://github.com/MichaelKosuke
+// Correctness 100% Performance 80%
+// Complexity O(N+M)
+
+class Solution {
+    public int[] solution(int N, int[] A) {
+        int n = A.Length;
+        int[] result = new int[N];
+        int maxval = 0;
+        bool justmax = true;
+        bool goingtomax = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (A[i] <= N)
+            {
+                result[A[i] - 1] += 1;
+                justmax = false;
+                if (goingtomax)
+                {
+                    maxval++;
+                }
+                else
+                {
+                    if (result[A[i] - 1] > maxval)
+                        maxval = result[A[i] - 1];
+                }
+            }
+            else
+            {
+                if (!justmax)
+                {
+                    if (i + 2 > n - 1 || (i + 2 <= n - 1 && A[i + 2] != N+1))
+                    {
+                        goingtomax = false;
+                        for (int j = 0; j < N; j++)
+                        {
+                            result[j] = maxval;
+                        }
+                        justmax = true;
+                    }
+                    else
+                    {
+                        goingtomax = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
